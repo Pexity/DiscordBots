4545ea6898461ab3d4e225e35377b38e015de0da
Pexity <42585153+Pexity@users.noreply.github.com>
2018-08-21 20:18:27 +0100

    d

---
+++ commands/poll.js
@@ -0,0 +1,21 @@
+// Require Statements | Makes the bot not work unless these are in the bot folder
+const Discord = require("discord.js");
+const config = require("../config.json");
+
+module.exports.run = async (bot, message, args) => {
+
+let msg = args.join(" "); 
+
+let pollmessage = new Discord.RichEmbed();
+.setTitle("**Poll**")
+.setDescription(msg)
+.setColor(config.embedcolor);
+
+let messageuser = await bots.channels.get('481540083783303188').send(polmessage);
+await messageuser.react('✅');
+await messageuser.react('❎');
+}
+
+module.exports.help = {
+  name: "poll"
+}
