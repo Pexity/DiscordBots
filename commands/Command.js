1da4b474a0d6bf68f5da38cf4af7ef8127360997
Pexity <42585153+Pexity@users.noreply.github.com>
2018-08-21 19:59:01 +0100

    d

--- commands/Command.js
+++ commands/Command.js
@@ -1,18 +1,18 @@
-// Require Statements
-const Discord = require(?discord.js);
+// Require Statements | Makes the bot not work unless these are in the bot folder
+const Discord = require("discord.js");
 const config = require("../config.json");
 
-module.exports.run = async (bot,messsage,args) => {
+module.exports.run = async (bot, message, args) => {
 
   let firstmessage = new Discord.RichEmbed()
   .setTitle("**Message**")
-  .setDescription("This is an Edited message that has no footer. An Embed Message is a Discord message type.")
+  .setDescription("This is an Embed message that has no footer. An Embed Message is a Discord message type.")
   .setColor(config.embedcolor);
 
   message.channel.send(firstmessage);
 
-  let secondmessage = new Discord.Richembed()
-  .setTitle("Another Message")
+  let secondmessage = new Discord.RichEmbed()
+  .setTitle("**Another Message**")
   .setDescription("This is another embed message that has a footer and does not have a bold title.")
   .setColor(config.embedcolor)
   .setTimestamp()
@@ -24,14 +24,14 @@ module.exports.run = async (bot,messsage,args) => {
 
   let message1 = new Discord.RichEmbed()
   .setTitle("**PM Message**")
-  .setDescription("I can pm you to:D")
+  .setDescription("I can pm you to :D")
   .setColor(config.embedcolor);
 
   message.author.send(message1);
-  
-  let message2 = message.author.send("I can pm this way too.")
+
+  let message2 = message.author.send("I can pm this way too.");
 }
 
-module.exsports.help = {
+module.exports.help = {
   name: "command"
 }
